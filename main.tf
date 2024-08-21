c4892b488cdaea34f139953f0cc6064495e448af
hrisimov <140465371+hrisimov@users.noreply.github.com>
2024-08-21 23:35:22 +0300

    File main.tf formatted

--- main.tf
+++ main.tf
@@ -6,10 +6,10 @@ terraform {
     }
   }
   backend "azurerm" {
-    resource_group_name = "storagerg"
+    resource_group_name  = "storagerg"
     storage_account_name = "taskboardstoragehrisim"
-    container_name = "taskboardcontainer"
-    key = "terraform.tfstate"
+    container_name       = "taskboardcontainer"
+    key                  = "terraform.tfstate"
   }
 }
 
